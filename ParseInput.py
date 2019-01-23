c431bbf67345fc1db001d3f02faf0484981c1ab8
Tomasz Skora <tskora93@gmail.com>
2019-01-23 15:11:34 +0100

    Deleted files removed from repository

--- ParseInput.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-def parse_input_filename():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_filename")
-    args = parser.parse_args()
-    return args.input_filename
