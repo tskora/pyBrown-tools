f2f2756ac9b1a8b1decad98498aa706f4dcffca1
Tomasz Skora <tskora93@gmail.com>
2019-06-11 09:31:03 +0200

    first version of xyz to str translating script added

---
+++ Xyz_to_str.py
@@ -0,0 +1,88 @@
+# pyBrown is a bound of tools useful for Brownian and Stokesian dynamics simulations
+# Copyright (C) 2018  Tomasz Skora (tskora@ichf.edu.pl)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses.
+
+import click
+
+from pyBrown.input import InputData
+from pyBrown.messaging import timestamp
+
+#-------------------------------------------------------------------------------
+
+@click.command()
+@click.argument('input_filename',
+				type = click.Path( exists = True ))
+def main(input_filename):
+
+	# here the list of keywords that are required for program to work is provided
+	# required_keywords = ["labels", "sizes", "box_size", "temperature", "viscosity",
+	# 					 "input_xyz_template", "input_enr_template", "input_xyz_range",
+	# 					 "input_enr_range"]
+	required_keywords = []
+
+	# here the dict of keywords:default values is provided
+	# if given keyword is absent in JSON, it is added with respective default value
+	# defaults = {"debug": False, "verbose": False, "fit_MSD": False,
+	# 			"probing_frequency": 1}
+	defaults = {}
+
+	timestamp( 'Reading input from {} file', input_filename )
+	i = InputData(input_filename, required_keywords, defaults).input_data
+
+	print(i)
+
+	xyz_file = open(i["input_xyz_filename"], 'r')
+	template_file = open(i["template_str_filename"], 'r')
+	str_file = open(i["output_str_filename"], 'w')
+
+	labels = []
+	coords = []
+
+	start_snapshot = False
+
+	for line in xyz_file:
+		if start_snapshot:
+			if 'time' in line.split():
+				start_snapshot = False
+			else:
+				if len( line.split() ) == 4:
+					labels.append( line.split()[0] )
+					coords.append( [ line.split()[i] for i in range(1,4) ] )
+		if 'time' in line.split():
+			if float(line.split()[-1]) == i["snapshot_time"]:
+				start_snapshot = True
+
+	for j, line in enumerate( template_file ):
+		if line.split()[0] == 'sub':
+			line_elements = line.split()
+			line_elements[3] = coords[j][0]
+			line_elements[4] = coords[j][1]
+			line_elements[5] = coords[j][2]
+			new_line = ''
+			for i in range( len(line_elements) ): new_line += line_elements[i] + ' '
+			new_line += '\n'
+			str_file.write(new_line)
+		else:
+			str_file.write(line)
+
+
+	xyz_file.close()
+	template_file.close()
+	str_file.close()
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+	main()
