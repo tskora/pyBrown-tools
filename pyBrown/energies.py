5e7011d917a63259797757fa442ee7405436a505
Tomasz Skora <tskora93@gmail.com>
2020-02-27 12:07:58 +0100

    hotfix: missing imports added

--- pyBrown/energies.py
+++ pyBrown/energies.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see https://www.gnu.org/licenses.
 
+from pyBrown.messaging import timestamp
+from pyBrown.plot_config import plot_config
+
 #-------------------------------------------------------------------------------
 
 def read_energies(input_data):
