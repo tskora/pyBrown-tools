1a4891a7e51cdab8d248bfd5134d586f81cd8fcc
Tomasz Skora <tskora93@gmail.com>
2020-03-12 09:03:24 +0100

    first version, witout printing

--- Pores.py
+++ Pores.py
@@ -16,10 +16,61 @@
 
 import click
 
+import numpy as np
+
 from pyBrown.input_Pores import InputDataPores
 from pyBrown.messaging import timestamp
 from pyBrown.trajectories import read_trajectories, add_auxiliary_data_multibeads
 from pyBrown.sphere import Sphere, overlap
+
+#-------------------------------------------------------------------------------
+
+def what_radius_based_on_label(label, labels, radii):
+
+	i = 0
+
+	for l, r in zip( labels, radii ):
+
+		if label == l: return radii[i][0]
+
+		i += 1
+
+#-------------------------------------------------------------------------------
+
+def digitize_grid(grid, populated_box, dx, box_size):
+
+	all_points = 0
+	overlapping_points = 0
+
+	for point in grid:
+
+		all_points += 1
+		radius = 0.5 * dx
+
+		with Sphere(point, radius) as probe:
+
+			if overlap( probe, populated_box, 0.0 ):
+				overlapping_points += 1
+				continue
+			else:
+				versors = [ np.array([nx * box_size,
+									  ny * box_size,
+									  nz * box_size])
+							for nx in np.arange(-1, 2, 1)
+							for ny in np.arange(-1, 2, 1)
+							for nz in np.arange(-1, 2, 1) ]
+
+				if_overlap = False
+
+				for versor in versors:
+					probe.translate( versor )
+					if overlap(probe, populated_box, 0.0):
+						if_overlap = True
+					probe.translate( -versor )
+				if if_overlap:
+					overlapping_points += 1
+
+	return overlapping_points, all_points, overlapping_points/all_points
 
 #-------------------------------------------------------------------------------
 
@@ -29,7 +80,8 @@ from pyBrown.sphere import Sphere, overlap
 def main(input_filename):
 
 	# here the list of keywords that are required for program to work is provided
-	required_keywords = ["labels", "box_size", "input_xyz_template", "input_xyz_range"]
+	required_keywords = ["labels", "box_size", "input_xyz_filename", "snapshot_time",
+						 "grid_density", "hydrodynamic_radii"]
 
 	# here the dict of keywords:default values is provided
 	# if given keyword is absent in JSON, it is added with respective default value
@@ -39,58 +91,53 @@ def main(input_filename):
 	i = InputDataPores(input_filename, required_keywords, defaults)
 	timestamp( 'Input data:\n{}', i )
 
-	timestamp( 'Reading trajectories' )
-	times, labels, auxiliary_data = read_trajectories(i.input_data)
-	add_auxiliary_data_multibeads( i.input_data, labels, auxiliary_data )
+	xyz_file = open(i.input_data["input_xyz_filename"], 'r')
 
-	import numpy as np
+	populated_box = []
 
-	N = 100
+	start_snapshot = False
+
+	for line in xyz_file:
+		if start_snapshot:
+			if 'time' in line.split():
+				start_snapshot = False
+			else:
+				if len( line.split() ) == 4:
+					label = line.split()[0]
+					radius = what_radius_based_on_label(label, i.input_data["labels"], i.input_data["hydrodynamic_radii"])
+					s = Sphere( [ line.split()[i] for i in range(1,4) ], radius )
+					populated_box.append( s )
+		if 'time' in line.split():
+			if float(line.split()[-1]) == i.input_data["snapshot_time"]:
+				start_snapshot = True
+
+	N = i.input_data["grid_density"]
 	dx = i.input_data["box_size"] / N
 
-	grid = [ [x, y, z] for x in np.linspace( -i.input_data["box_size"], i.input_data["box_size"], N )
-					   for y in np.linspace( -i.input_data["box_size"], i.input_data["box_size"], N )
-					   for z in np.linspace( -i.input_data["box_size"], i.input_data["box_size"], N ) ]
+	grid = [ [x, y, z] for x in np.linspace( -i.input_data["box_size"]/2, i.input_data["box_size"]/2, N )
+					   for y in np.linspace( -i.input_data["box_size"]/2, i.input_data["box_size"]/2, N )
+					   for z in np.linspace( -i.input_data["box_size"]/2, i.input_data["box_size"]/2, N ) ]
 
+	import multiprocessing
+	from multiprocessing import Pool
+	from functools import partial
 
-	for point in grid:
-		radius = 0.5 * dx
-		with Sphere(point, radius) as probe:
+	nproc = multiprocessing.cpu_count()
+	print('You have {0:1d} CPUs'.format(nproc))
 
-			if overlap( probe, populated_box, 0.0 ):
-				print(1)
-				continue
+	points_per_proc = len(grid) // nproc
+	grid_for_proc = [ grid[m*points_per_proc:(m+1)*points_per_proc] for m in range(nproc - 1) ] \
+					+ [ grid[(nproc-1)*points_per_proc:] ]
 
-			else:
-                versors = [ np.array([nx * i.input_data["box_size"][0],
-                                      ny * i.input_data["box_size"][1],
-                                      nz * i.input_data["box_size"][2]])
-                            for nx in np.arange(-1, 2, 1)
-                            for ny in np.arange(-1, 2, 1)
-                            for nz in np.arange(-1, 2, 1) ]
+	pool = Pool(processes=nproc)
 
-                if_overlap = False
+	digitize_grid_partial = partial( digitize_grid, populated_box = populated_box, dx = dx, box_size = i.input_data["box_size"] )
 
-                for versor in versors:
+	print(digitize_grid_partial(grid))
 
-                    probe.translate( versor )
-
-                    if overlap(probe, populated_box, 0.0):
-                        if_overlap = True
-
-                    probe.translate( -versor )
-
-                if if_overlap: print(1)
-            	else: print(0)
-
-	# timestamp( 'Computing radial distribution function' )
-	# bins, rdfs, distinct_rdf = compute_rdfs( i.input_data, labels, auxiliary_data )
-
-	# timestamp( 'Saving radial distribution function to a file' )
-	# save_rdfs_to_file(i.input_data, bins, rdfs, distinct_rdf)
-
-	del times
-	# del msds
+	count = pool.map( digitize_grid_partial, grid_for_proc )
+	
+	print(count)
 
 #-------------------------------------------------------------------------------
 
