ec025eb8e1140e9df2f20178a9d95e5b8a6a663f
Tomasz Skora <tskora93@gmail.com>
2020-03-17 00:28:10 +0100

    _

--- ex_vol.py
+++ ex_vol.py
@@ -89,8 +89,10 @@ if __name__ == '__main__':
 	input_labels = ["FIC", "DNA", "DNS"]
 	input_radii = [51.0, 11.4, 39.2]
 
-	which = 3
-	xyz_filenames = [ 'ficoll_42_'+str(which)+'.xyz', 'ficoll_41_DNA_14_'+str(which)+'.xyz', 'ficoll_39_DNA_39_'+str(which)+'.xyz', 'ficoll_35_DNA_104_'+str(which)+'.xyz' ]
+	xyz_filenames = []
+	for which in range(1, 4):
+		xyz_filenames += [ 'ficoll_39_DNA_39_'+str(which)+'.xyz' ]
+		# xyz_filenames += [ 'ficoll_42_'+str(which)+'.xyz', 'ficoll_41_DNA_14_'+str(which)+'.xyz', 'ficoll_39_DNA_39_'+str(which)+'.xyz', 'ficoll_35_DNA_104_'+str(which)+'.xyz' ]
 	# xyz_filenames = [ 'ficoll_41_DNA_14_'+str(which)+'.xyz', 'ficoll_39_DNA_39_'+str(which)+'.xyz', 'ficoll_35_DNA_104_'+str(which)+'.xyz' ]
 	# xyz_filenames = [ 'ficoll_18_DNS_54_'+str(which)+'.xyz' ]
 	# xyz_filenames = [ 'ficoll_18_DNS_54_'+str(which)+'.xyz' ]
@@ -104,8 +106,6 @@ if __name__ == '__main__':
 		print(xyz_filename)
 
 		for time in times:
-
-			print(time)
 
 			with open(xyz_filename, 'r') as xyz_file:
 
@@ -124,18 +124,6 @@ if __name__ == '__main__':
 			crowders = place_crowders_xyz(r_crowders, snapshot)
 
 			crowders = crowders[:-1]
-
-	# def sphere_volume(radius):
-	#     return 4 / 3 * np.pi * radius**3
-	
-	# def sphere_surface(radius):
-	#     return 4 * np.pi * radius**2
-	
-	# def cylinder_volume(radius, length):
-	#     return np.pi * radius**2 * length
-	
-	# def excluded_volume_sphere_Nspheres(radius_tracer, radius, N):
-	#     return N * sphere_volume(radius) + sphere_volume(radius_tracer) + N * sphere_surface(radius) * radius_tracer + (N + 1) * cylinder_volume(radius_tracer, 2 * radius)
 	
 			count = 0
 
@@ -148,64 +136,29 @@ if __name__ == '__main__':
 					tracer.r -= 1.5
 				if overlap(crowders, tracers, 0.0):
 					count += 1
-	    		# print( '{}\t{}'.format(i + 1, count / (i + 1) * box_size**3) )
+				else:
+					versors = [ np.array([nx * box_size,
+										  ny * box_size,
+										  nz * box_size])
+								for nx in np.arange(-1, 2, 1)
+								for ny in np.arange(-1, 2, 1)
+								for nz in np.arange(-1, 2, 1) ]
+
+					if_overlap = False
+
+					for versor in versors:
+
+						for tracer in tracers:
+							tracer.translate( versor )
+
+						if overlap(crowders, tracers, 0.0):
+							if_overlap = True
+
+						for tracer in tracers:
+							tracer.translate( -versor )
+
+					if if_overlap:
+						count += 1
 	
 			ex_vol = count / number_of_trials * box_size**3
 			print(ex_vol / box_size**3)
-
-	# print(ex_vol)
-	# print( excluded_volume_sphere_Nspheres(2.289, 1.14, 8) )
-	
-	### SOME TEST CASES
-	
-	# s_crowders = [Sphere([0.0, 0.0, -r_crowder], r_crowder), Sphere([0.0, 0.0, r_crowder], r_crowder)]
-	
-	# for i in range(number_of_trials):
-	
-	#     with Sphere(mc.get_values(), r_tracer) as s_tracer:
-	#         if any([overlap(s_crowder, s_tracer) for s_crowder in s_crowders]):
-	#             count += 1
-	
-	# ex_vol = count / number_of_trials * box_size**3
-	# expected = 2 * 4 / 3 * np.pi * (r_tracer + r_crowder)**3 - 4 / 3 * np.pi * r_tracer**3 - 2 * np.pi * r_crowder * r_tracer**2
-	
-	# print('Computed excluded volume:\t{}'.format(ex_vol))
-	# print('Expected excluded volume:\t{}'.format(expected))
-	
-	# ###
-	
-	# box_size = 10.0
-	# r_crowder = 2.0
-	# r_tracer = 2.0
-	# number_of_trials = 1000000
-	
-	# mc = MonteCarlo(False, box_size)
-	# count = 0
-	
-	# s_crowder = Sphere([0.0, 0.0, 0.0], r_crowder)
-	
-	# for i in range(number_of_trials):
-	
-	#     shot = mc.get_values()
-	
-	#     with Sphere(shot[0:3], r_tracer) as s1, Sphere(shot[0:3], r_tracer) as s2:
-	
-	#         s_tracers = [s1, s2]
-	#         s1.translate([0.0, 0.0, r_crowder])
-	#         s2.translate([0.0, 0.0, -r_crowder])
-	
-	#         for s in s_tracers:
-	#             angles = list(shot[3:5]) + [0.0]
-	#             s.rotate(angles)
-	
-	#         # if any([overlap(s_crowder, s_tracer) for s_tracer in s_tracers]):
-	#         #     count += 1
-	
-	#         if overlap(s_crowder, s_tracers):
-	#             count += 1
-	
-	# ex_vol = count / number_of_trials * box_size**3
-	# expected = 2 * 4 / 3 * np.pi * (r_tracer + r_crowder)**3 - 4 / 3 * np.pi * r_tracer**3 - 2 * np.pi * r_crowder * r_tracer**2
-	
-	# print('Computed excluded volume:\t{}'.format(ex_vol))
-	# print('Expected excluded volume:\t{}'.format(expected))	
