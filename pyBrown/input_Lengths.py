acc6e4346b2c114097577a0df3a98c9394df2a30
Tomasz Skora <tskora93@gmail.com>
2020-04-08 11:15:23 +0200

    _

---
+++ pyBrown/input_Lengths.py
@@ -0,0 +1,29 @@
+# pyBrown is a bundle of tools useful for Brownian and Stokesian dynamics
+# simulations. Copyright (C) 2018  Tomasz Skora (tskora@ichf.edu.pl)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses.
+
+import json
+
+from pyBrown.input import InputData
+
+#-------------------------------------------------------------------------------
+
+class InputDataLengths(InputData):
+
+    def __init__(self, input_filename, obligatory_keywords = [], defaults = {}):
+
+        super().__init__(input_filename, obligatory_keywords, defaults)
+    
+    #---------------------------------------------------------------------------
