c8664d2b441ef41c32dd24155357ed8a3c5f830f
Tomasz Skora <tskora93@gmail.com>
2022-08-18 23:36:05 +0200

    license added

--- pyBrown_tools/fluxes.py
+++ pyBrown_tools/fluxes.py
@@ -1,3 +1,19 @@
+# pyBrown is a bundle of tools useful for Brownian and Stokesian dynamics
+# simulations. Copyright (C) 2018  Tomasz Skora (tskora@ichf.edu.pl)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see https://www.gnu.org/licenses.
+
 import numpy as np
 
 def compute_flux_single(r0, r1, plane_normal_vector, plane_point):
